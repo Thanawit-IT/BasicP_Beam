35c7af8ee61a98d3804e514278fb504fbad5c85b
Thanawit-IT <beamniceking@gmail.com>
2025-07-23 11:16:46 +0700

    Add class3

---
+++ class3.py
@@ -0,0 +1,57 @@
+# Have_Rice = True
+# Have_Spoon = False
+# Have_Hand = True
+
+# if Have_Rice : 
+#     if Have_Spoon : 
+#         print("กินข้าว")
+#     elif Have_Hand : 
+#         print("กินข้าวเหนียว")
+
+
+# Score = int(input("Enter Score : "))
+
+# if Score >= 0 :
+#     if Score <= 100 :
+#         if Score >= 80 :
+#             print("Grade A")
+#         if Score >= 70 :
+#             if Score < 80 :
+#                     print("Grade B")
+#         if Score >= 60 :
+#             if Score < 70 :
+#                     print("Grade C")
+#         if Score >= 50 :
+#             if Score < 60 :
+#                     print("Grade D")
+#         if Score < 50 :
+#                 print("Grade F")
+
+# for i in range(1,10,2) :
+#     print(i)
+
+# i = 0
+
+# while i < 8 :
+#     print("S")
+#     i = i + 1
+
+# while True :
+#     choice = int(input("กรอก 1 เพื่อบวกเลข, กรอก 2 เพื่อออก : "))
+
+#     if choice == 1 :
+#         num = int(input("จน.เลขที่ต้องการจะบวก : "))
+#         sum = 0 
+#         for i in range(num):
+#             num1 = int(input("กรอกเลข : "))
+#             sum = sum + num1
+#         print("ผลลัพธ์ : ",sum)
+#         break
+#     if choice == 2 :
+#         print("End Program")
+#         break
+
+
+
+
+                
